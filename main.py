d8c349b43f9853d9b30eae796dd91ed13b9143e2
Ricardo-we <totigamer75@gmail.com>
2023-06-07 13:52:23 -0600

    Completed

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from tkinter.messagebox import askyesno, showinfo
 # Javascript to get onlyinfo
 # Array.from(document.querySelectorAll(".b-photos__item__img")).map(img => img?.src)
 # const d = new Set()
-data_file = open("data/alina.json")
-urls = json.load(data_file)
+# data_file = open("data/alina.json")
+# urls = json.load(data_file)
 
-IMAGES_PREFIX = "alina_rose"
-# result_dir = "D:\\totig\\totig\\Imagenes\\Only\\alina_rose"
-result_dir = f"C:\\Users\\ricar\\OneDrive\\Imágenes\\Only\\{IMAGES_PREFIX}"
+# IMAGES_PREFIX = "alina_rose"
+# # result_dir = "D:\\totig\\totig\\Imagenes\\Only\\alina_rose"
+# result_dir = f"C:\\Users\\ricar\\OneDrive\\Imágenes\\Only\\{IMAGES_PREFIX}"
 
-def save_image(image_content, dir, name):
-    with open(dir + f"\\{IMAGES_PREFIX}-{name}.jpg", 'wb') as img:
+def save_image(image_content, dir, name, image_prefix):
+    with open(dir + f"\\{image_prefix}-{name}.jpg", 'wb') as img:
         img.write(image_content)
 
 def main():
@@ -27,20 +27,22 @@ def main():
         finished = askyesno("Los datos introducidos no son validos","¿Desea salir?")
         if finished: return
         return main()
-    showinfo("El resultado será", f"Origen (Json): {data_file}\n" + f"Resultado: {data_file}")
+    showinfo("El resultado será", f"Origen (Json): {data_file.name}\n" + f"Resultado: {result_dir}")
     data_file = open(data_file.name)
     urls = json.load(data_file)
+    image_prefix = result_dir.split("\\")[0].split("/")[-1]
 
     for url in urls:
         try:
             response = requests.get(url, stream=True, allow_redirects=True)
             image_content = response.content
-            save_image(image_content, result_dir, str(datetime.datetime.now().time().microsecond + random.random() *100))
-        except: 
-            print("Error")
-
+            save_image(image_content, result_dir, str(datetime.datetime.now().time().microsecond + random.random() *100), image_prefix)
+        except Exception as e: 
+            print("Error", e)
+            
+    showinfo("Proceso completado")
     repeat = askyesno("Proceso completado","¿Desea realizar otra descarga?")
-
+    print(repeat)
     if repeat: 
         main()
 
